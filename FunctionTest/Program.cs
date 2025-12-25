265ac4bfd790b900b2dd9f2f0af28223168f793d
Shizuka7 <S_Shizuka@outlook.com>
2025-12-25 16:27:49 +0800

    git sync

--- FunctionTest/Program.cs
+++ FunctionTest/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using Bakend.Module.DataBaseModule;
+﻿using Backend.Module.DataBaseModule;
 using ConfigManager;
 using EventModule;
 using Module;
@@ -11,12 +10,12 @@ public static class Program
     public static void Main(string[] args)
     {
         //ModuleCenter.SCreateModule<ConfigModule>();
-        ModuleCenter.InitModules<ConfigModule>();
+        //ModuleCenter.InitModules<ConfigModule>();
 
         Logger.Log($"{ConfigModule.Setting.Port}");
         Logger.Log($"{ConfigModule.Setting.PostgresDbPassword}");
         Logger.Log($"{ConfigModule.Setting.RedisPassword}");
-        ModuleCenter.InitModules<DataBaseModule>();
+        //ModuleCenter.InitModules<DataBaseModule>();
         //  ConfigModule.Instance.Save();
 
     }
